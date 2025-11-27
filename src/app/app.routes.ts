b46f50a7f615f48cf4c1b24b8c4442965fd5ca89
sgarrui1201 <sgarrui1201@g.educaand.es>
2025-11-27 10:11:55 +0100

    stephen MCpollo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,30 +4,36 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './core/guards/auth.guard';
 import { RegisterComponent } from './pages/register/register.component';
 import { CentersComponent } from './pages/centers/centers.component';
+// Importamos el componente de máquinas (lo crearemos en el siguiente paso)
+import { MachinesComponent } from './pages/machines/machines.component';
 
 export const routes: Routes = [
     {
-        path:'',
-        redirectTo:'dashboard',
-        pathMatch:'full'
-
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
     },
     {
-        path:'login',
-        component:LoginComponent
+        path: 'login',
+        component: LoginComponent
     },
     {
-        path:'register',
-        component:RegisterComponent
+        path: 'register',
+        component: RegisterComponent
     },
     {
-        path:'dashboard',
-        component:DashboardComponent,
-        canActivate:[authGuard]
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [authGuard]
     },
     {
-        path:'centers',
-        component:CentersComponent,
-        canActivate:[authGuard]
+        path: 'centers',
+        component: CentersComponent,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'machines',
+        component: MachinesComponent, // Asegúrate de crear este componente o dará error
+        canActivate: [authGuard]
     }
 ];
