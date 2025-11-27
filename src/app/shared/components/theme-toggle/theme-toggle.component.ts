d476df53a081e5152b3cd530c35a944925fc5a57
Mariogarluu <garcialuquemario7@gmail.com>
2025-11-27 19:38:53 +0100

    ElYoya Type Commit

---
+++ src/app/shared/components/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../../core/services/theme.service';
+
+@Component({
+  selector: 'app-theme-toggle',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './theme-toggle.component.html',
+  styleUrl: './theme-toggle.component.scss'
+})
+export class ThemeToggleComponent {
+  themeService = inject(ThemeService);
+
+  theme = this.themeService.theme;
+  isDark = this.themeService.isDark;
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
+}
+
