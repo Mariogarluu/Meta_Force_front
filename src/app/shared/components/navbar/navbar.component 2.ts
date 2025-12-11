191edd5fa671d140932903dea62f79dce8637d39
Mario García Luque <garcialuquemario7@gmail.com>
2025-12-11 09:18:54 +0100

    Juan Antonio's update

--- src/app/shared/components/navbar/navbar.component 2.ts
+++ src/app/shared/components/navbar/navbar.component 2.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-navbar2',
   standalone: true,
   imports: [CommonModule, RouterLink, RouterLinkActive],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  templateUrl: './navbar.component 2.html',
+  styleUrl: './navbar.component 2.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent2 {
   readonly navLinks = [
     { label: 'Inicio', path: '/' },
     { label: 'Clases', path: '/dashboard' },
